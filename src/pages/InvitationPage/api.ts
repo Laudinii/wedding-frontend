7568b0c36e5247f3ffbe5b116f1d7422a8172a4c
Kamil Krzeminski <krzeminski.projects@gmail.com>
2025-05-31 13:13:07 +0200

    dodo

--- src/pages/InvitationPage/api.ts
+++ src/pages/InvitationPage/api.ts
@@ -3,9 +3,9 @@ import axios from "axios";
 export const getInvitation = async (id: string | undefined) => {
   if (!id) return;
 
-  return (
-    await axios.get(
-      `https://wedding-backend-kkv5.onrender.com/invitations/${id}`
-    )
-  ).data;
+  // return (
+  //   await axios.get(
+  //     `https://wedding-backend-kkv5.onrender.com/invitations/${id}`
+  //   )
+  // ).data;
 };
